b1bc83f07fe211b9e621fcbbd114b229d0274034
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-02-26 17:27:59 -0300

    corrigindo erros nas infos

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
 var data;
 function getanswer(g) {
 
-    $.get("http://www.omdbapi.com/?s=" + g + "&apikey=e4e957f", function (rawdata) {
+    $.get("http://www.omdbapi.com/?t=" + g + "&apikey=e4e957f", function (rawdata) {
         var rawstring = JSON.stringify(rawdata);
         data = JSON.parse(rawstring);
         console.log(data);
         //console.log("resultados:" + data.totalResults);
         var resultados = data.totalResults;
-        var title = data.Search[0].Title;
-        var year = data.Search[0].Year;
-        var runtime = data.Search[0].Runtime;
-        var genre = data.Search[0].Genre;
-        var website = data.Search[0].Website;
-        var imdburl = "https://www.imdb.com/title/" + data.Search[0].imdbID + "/";
-        var posterurl = data.Search[0].Poster;
+        var title = data.Title;
+        console.log(title);
+        var type = data.Type;
+        console.log(type);
+        //var year = data.Search[0].Year;
+        var year = data.Year;
+        var runtime = data.Runtime;
+        var genre = data.Genre;
+        var website = data.Website;
+        var imdburl = "https://www.imdb.com/title/" + data.imdbID + "/";
+        var posterurl = data.Poster;
         document.getElementById("resposta").innerHTML =
-          //  "<h3> Resultados: " + resultados + "</h3>"+
-            "<h1>" + title + "</h1>"+
-            "<p>Year Realised: " + year + "</p>"+
-            "<p> Minutos: " + runtime + "</p>"+
-            "<p> Gênero: " + genre + "</p>" + 
-            "<p> Site: " + website + "</p>"+
-            "<br><img src=" + posterurl + "/>" + 
+            //  "<h3> Resultados: " + resultados + "</h3>"+
+            "<h1>" + title + "</h1>" +
+            "<p> Tipo: " + type + "</p>" +
+            "<p>Year Realised: " + year + "</p>" +
+            "<p> Minutos: " + runtime + "</p>" +
+            "<p> Gênero: " + genre + "</p>" +
+            "<p> Site: " + website + "</p>" +
+            "<br><img src=" + posterurl + "/>" +
             "<p>IMDB page: <a href=" + imdburl + " target=_'blank'>" + imdburl + "</a></p>";
     })
 }
