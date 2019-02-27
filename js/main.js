369d90abbdb63f1459618c3224df308b71bd2341
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-02-27 19:48:22 -0300

    lidando com filmes não encontrados

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 
-var data;
-
 function lerCampo() {
     if ($("#campo").val() == "") {
         $("#resultado").removeClass("shown").addClass("hidden");
@@ -10,15 +8,13 @@ function lerCampo() {
 }
 
 function buscaFilme(consulta) {
-
-    /*
-
-    $.ajax({
-        url: "http://www.omdbapi.com/?t=" + consulta + "&apikey=e4e957f",
-        type: "GET",
-        success: function (rawdata) {
-            var rawstring = JSON.stringify(rawdata);
-            data = JSON.parse(rawstring);
+    var data;
+    $.get("http://www.omdbapi.com/?t=" + consulta + "&apikey=e4e957f", function (rawdata) {
+        var rawstring = JSON.stringify(rawdata);
+        data = JSON.parse(rawstring);
+        var resposta = data.Response;
+        console.log(resposta);
+        if (resposta == "True") {
             var title = data.Title;
             var type = data.Type;
             var year = data.Year;
@@ -37,36 +33,8 @@ function buscaFilme(consulta) {
                 "<p>Gênero: " + genre + "</p>" +
                 "<p>Site: <a href=" + website + " target=_'blank'>" + website + "</a></p>"
             $("#resultado").removeClass("hidden").addClass("shown");
-        },
-        error: function(rawdata){
-            console.log("erro");
+        } else if(resposta == "False"){
+            document.getElementById("resultado").innerHTML = "<h2> Não encontramos nada com esse nome... :( </h2>"
         }
     })
-    */
-
-    $.get("http://www.omdbapi.com/?t=" + consulta + "&apikey=e4e957f", function (rawdata) {
-        var rawstring = JSON.stringify(rawdata);
-        data = JSON.parse(rawstring);
-        var title = data.Title;
-        var type = data.Type;
-        var year = data.Year;
-        var runtime = data.Runtime;
-        var genre = data.Genre;
-        var website = data.Website;
-        var imdburl = "https://www.imdb.com/title/" + data.imdbID + "/";
-        var posterurl = data.Poster;
-
-        document.getElementById("resultado").innerHTML =
-            "<br><img src=" + posterurl + "/>" +
-            "<h2>" + title + "</h2>" +
-            "<p>Tipo: " + type + "</p>" +
-            "<p>Ano de Lançamento " + year + "</p>" +
-            "<p>Minutos: " + runtime + "</p>" +
-            "<p>Gênero: " + genre + "</p>" +
-            "<p>Site: <a href=" + website + " target=_'blank'>" + website + "</a></p>"
-        $("#resultado").removeClass("hidden").addClass("shown");
-    }).fail(function(){
-        alert("erro");
-    })
 }
-
