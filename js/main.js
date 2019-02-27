893d1474b6e3a263c0b2d4fa1faae3032372b4e8
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-02-27 18:42:39 -0300

    lidando com campo vazio

--- js/main.js
+++ js/main.js
@@ -5,25 +5,31 @@ console.log(campo);
 
 function buscaFilme(consulta) {
 
-    $.get("http://www.omdbapi.com/?t=" + consulta + "&apikey=e4e957f", function (rawdata) {
-        var rawstring = JSON.stringify(rawdata);
-        data = JSON.parse(rawstring);
-        var title = data.Title;
-        var type = data.Type;
-        var year = data.Year;
-        var runtime = data.Runtime;
-        var genre = data.Genre;
-        var website = data.Website;
-        var imdburl = "https://www.imdb.com/title/" + data.imdbID + "/";
-        var posterurl = data.Poster;
+    if ($("#campo").val() == "") {
+        $("#resultado").removeClass("shown").addClass("hidden");
+    } else {
+        $.get("http://www.omdbapi.com/?t=" + consulta + "&apikey=e4e957f", function (rawdata) {
+            var rawstring = JSON.stringify(rawdata);
+            data = JSON.parse(rawstring);
+            var title = data.Title;
+            var type = data.Type;
+            var year = data.Year;
+            var runtime = data.Runtime;
+            var genre = data.Genre;
+            var website = data.Website;
+            var imdburl = "https://www.imdb.com/title/" + data.imdbID + "/";
+            var posterurl = data.Poster;
 
-        document.getElementById("resultado").innerHTML =
-            "<br><img src=" + posterurl + "/>" +
-            "<h2>" + title + "</h2>" +
-            "<p>Tipo: " + type + "</p>" +
-            "<p>Ano de Lançamento " + year + "</p>" +
-            "<p>Minutos: " + runtime + "</p>" +
-            "<p>Gênero: " + genre + "</p>" +
-            "<p>Site: <a href=" + website + " target=_'blank'>" + website + "</a></p>"
-    })
+            document.getElementById("resultado").innerHTML =
+                "<br><img src=" + posterurl + "/>" +
+                "<h2>" + title + "</h2>" +
+                "<p>Tipo: " + type + "</p>" +
+                "<p>Ano de Lançamento " + year + "</p>" +
+                "<p>Minutos: " + runtime + "</p>" +
+                "<p>Gênero: " + genre + "</p>" +
+                "<p>Site: <a href=" + website + " target=_'blank'>" + website + "</a></p>"
+            $("#resultado").removeClass("hidden").addClass("shown");
+        })
+    }
 }
+
