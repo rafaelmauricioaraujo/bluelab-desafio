b44f5e4f62f7aefd0ca68e98122f0f68beaa3bbd
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-02-27 08:55:40 -0300

    Melhorando o visual

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+
 var data;
+
+var tbody = document.querySelector("table tbody")
+console.log('tbody: '+ tbody);
+
+
 function getanswer(g) {
 
     $.get("http://www.omdbapi.com/?t=" + g + "&apikey=e4e957f", function (rawdata) {
@@ -8,9 +14,7 @@ function getanswer(g) {
         //console.log("resultados:" + data.totalResults);
         var resultados = data.totalResults;
         var title = data.Title;
-        console.log(title);
         var type = data.Type;
-        console.log(type);
         //var year = data.Search[0].Year;
         var year = data.Year;
         var runtime = data.Runtime;
@@ -18,6 +22,41 @@ function getanswer(g) {
         var website = data.Website;
         var imdburl = "https://www.imdb.com/title/" + data.imdbID + "/";
         var posterurl = data.Poster;
+
+        /*
+
+        var resposta = [
+            year,
+            runtime,
+            genre,
+            website
+        ];
+        */
+
+        //console.log("resposta: " + resposta);
+        //var tr = document.createElement("tr");
+        //var tdAno = document.createElement("tr");
+        //var tdRuntime = document.createElement("tr");
+        ///var tdGenre = document.createElement("tr");
+        //var tdWebsite document.createElement("tr");
+        //tdAno.textContent = data.Year;
+        //tdRuntime.textContent = data.Runtime;
+        //tdGenre.textContent = data.Genre;
+        //tdWebsite.textContent = data.Website;
+
+        /*
+        var tr = document.createElement("tr");
+        console.log('tr criada: ' + tr);
+        resposta.array.forEach(function (element) {
+            var td = document.createElement("td");
+            td.textContent = element.value;
+            tr.appendChild(td);
+        });
+        */
+        //console.log("tr: " + tr);
+        //tbody.appendChild(tr);
+        //console.log("tbody: " + tbody);
+        
         document.getElementById("resposta").innerHTML =
             //  "<h3> Resultados: " + resultados + "</h3>"+
             "<h1>" + title + "</h1>" +
@@ -29,4 +68,5 @@ function getanswer(g) {
             "<br><img src=" + posterurl + "/>" +
             "<p>IMDB page: <a href=" + imdburl + " target=_'blank'>" + imdburl + "</a></p>";
     })
+    
 }
