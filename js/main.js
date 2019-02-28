d2a94d8c6211ea954f24fadb0609b43836b1a2ae
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-02-28 00:11:09 -0300

    melhorando funcionalidades

--- js/main.js
+++ js/main.js
@@ -8,33 +8,42 @@ function lerCampo() {
 }
 
 function buscaFilme(consulta) {
-    var data;
+    let data;
     $.get("http://www.omdbapi.com/?t=" + consulta + "&apikey=e4e957f", function (rawdata) {
-        var rawstring = JSON.stringify(rawdata);
+        let rawstring = JSON.stringify(rawdata);
         data = JSON.parse(rawstring);
-        var resposta = data.Response;
-        console.log(resposta);
+        console.log(data);
+        let resposta = data.Response;
         if (resposta == "True") {
-            var title = data.Title;
-            var type = data.Type;
-            var year = data.Year;
-            var runtime = data.Runtime;
-            var genre = data.Genre;
-            var website = data.Website;
-            var imdburl = "https://www.imdb.com/title/" + data.imdbID + "/";
-            var posterurl = data.Poster;
+            let title = ((data.Title == "N/A") ? "Infelizmente não temos essa informação :( ": data.Title);
+            let year = ((data.Year == "N/A") ? "Infelizmente não temos essa informação :( " : data.Year);
+            let runtime = ((data.Runtime == "N/A") ? "Infelizmente não temos essa informação :( ": data.Runtime);
+            let genre = ((data.Genre == "N/A") ? "Infelizmente não temos essa informação :(" : data.Genre);
+            let website = ((data.Website == "N/A")? "Infelizmente não temos essa informação :(" : data.Website)
+            var posterurl = ((data.Poster == "N/A")? "Infelizmente não temos essa informação :(" : data.Poster);
 
             document.getElementById("resultado").innerHTML =
+                "<div class=row>" + 
+                "<div class=col-6>"+
                 "<br><img src=" + posterurl + "/>" +
+                "</div>" + 
+                "<div class=col-6>" + 
                 "<h2>" + title + "</h2>" +
-                "<p>Tipo: " + type + "</p>" +
                 "<p>Ano de Lançamento " + year + "</p>" +
                 "<p>Minutos: " + runtime + "</p>" +
                 "<p>Gênero: " + genre + "</p>" +
-                "<p>Site: <a href=" + website + " target=_'blank'>" + website + "</a></p>"
+                "<p>Site: <a href=" + website + " target=_'blank'>" + website + "</a></p>" + 
+                "</div>" + 
+                "</div>"
+            
             $("#resultado").removeClass("hidden").addClass("shown");
+
         } else if(resposta == "False"){
             document.getElementById("resultado").innerHTML = "<h2> Não encontramos nada com esse nome... :( </h2>"
         }
     })
+}
+
+function montaResposta(id, dados){
+    $(id).append("<br><img src=" + dados.Poster + ">");
 }
