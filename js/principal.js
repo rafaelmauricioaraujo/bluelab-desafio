e48a0e409dcfac365c614b8cd3d8def99385fcdf
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-03-01 01:14:43 -0300

    ajustes finais e comentários nas funções

--- js/principal.js
+++ js/principal.js
@@ -1,10 +1,10 @@
 
 function lerCampo() {
-    //console.log('acionado')
     /**
-     * função responsável por verificar se foi digitado alguma coisa 
+     * função responsável por verificar se foi digitado algo 
      * no campo de busca, caso sim, é chamado a função buscaFilme, caso não
-     * o espaço de resposta permanece oculto e sem nenhuma tag html.
+     * o espaço de resposta permanece oculto e qualquer tag html é removida
+     * para uma melhor visualização do site.
      */
     if ($("#campo").val() == "") {
         $("#resultado").removeClass("shown").addClass("hidden");
@@ -15,10 +15,10 @@ function lerCampo() {
 }
 
 function buscaFilme(filme) {
-    console.log('acionado também');
     /**
      * Função responsável por fazer uma requisição a API de filmes e retornar um objeto
-     * JSON para ter informações sobre os filmes mapeadas e enviadas à pagina.
+     * JSON contendo informações sobre o filme pesquisado
+     * e atualizar página com estas informações.
      * 
      * entrada: nome do filme ou parte do nome
      */
@@ -34,25 +34,32 @@ function buscaFilme(filme) {
         let resposta = data.Response;
         if (resposta == "True") {
 
-            let title = ((data.Title == "N/A") ? "Infelizmente não temos essa informação :( ": data.Title);
+            let title = ((data.Title == "N/A") ? "Infelizmente não temos essa informação :( " : data.Title);
             let year = ((data.Year == "N/A") ? "Infelizmente não temos essa informação :( " : data.Year);
-            let runtime = ((data.Runtime == "N/A") ? "Infelizmente não temos essa informação :( ": data.Runtime);
+            let runtime = ((data.Runtime == "N/A") ? "Infelizmente não temos essa informação :( " : data.Runtime);
             let genre = ((data.Genre == "N/A") ? "Infelizmente não temos essa informação :(" : data.Genre);
-            let website = ((data.Website == "N/A")? "Infelizmente não temos essa informação :(" : data.Website)
-            let posterurl = ((data.Poster == "N/A")? "Infelizmente não temos essa informação :(" : data.Poster);
+            let website = ((data.Website == "N/A") ? "Infelizmente não temos essa informação :(" : data.Website)
+            let posterurl = ((data.Poster == "N/A") ? "":data.Poster);
 
+            /**
+             *  O código abaixo captura o campo de resultado e preenche com os campos mapeados do JSON
+             * solicitado
+             */
             $("#resultado").html
-            ("<h2>" + title + "</h2>" +
-            "<img src="+posterurl + "/>"+
-            "<p> Ano de Lançamento: " + year + "</p>" +
-            "<p> Minutos: " + runtime + "</p>" + 
-            "<p> Gênero: " + genre + "</p>" + 
-            "<p> Site: <a href=" + website + " target='_blank'>" + website + "</a></p>"
-            );
-            
+                ("<h2>" + title + "</h2>" +
+                    "<img src=" + posterurl + "/>" +
+                    "<p> Ano de Lançamento: " + year + "</p>" +
+                    "<p> Minutos: " + runtime + "</p>" +
+                    "<p> Gênero: " + genre + "</p>" +
+                    "<p> Site: <a href=" + website + " target='_blank'>" + website + "</a></p>"
+                );
+
             $("#resultado").removeClass("hidden").addClass("shown");
 
-        } else if(resposta == "False"){
+        } else if (resposta == "False") {
+            /**
+             * resposta de filme não encontrado caso o campo resposta seja igual a False;
+             */
             $("#resultado").html("<h2> Não encontramos filme com esse nome... :( </h2>");
         }
     })
