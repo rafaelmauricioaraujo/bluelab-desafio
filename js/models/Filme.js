2d6a7a10ad734bd97f4e92a46a535bf1585634a0
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-03-01 01:16:26 -0300

    removendo arquivos desnecessarios

--- js/models/Filme.js
+++
@@ -1,9 +0,0 @@
-class Filme {
-    constructor(title, year, runtime, genre, website){
-        this.title = title;
-        this.year = year;
-        this.runtime = runtime;
-        this.genre = genre;
-        this.website = website;
-    }
-}
